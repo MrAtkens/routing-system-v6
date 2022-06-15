c63d0573609c2e591227da84604dfb759a33ecd4
our ame <r.kaliaskar@mail.ru>
2022-06-15 21:12:30 +0600

    Header edited

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export const Header = () => {
     return (
         <header>
-            <h1>Sticky Header Pow!</h1>
+            <h1>Sticky Header Pow 242134234234!</h1>
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/app">About</Link>
